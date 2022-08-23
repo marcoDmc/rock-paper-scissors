10b4495fa77c41e92a2ad960006c1bfd95aaa44a
marcoDmc <marcodamasceno07@gmail.com>
2022-08-23 20:00:04 -0300

    :art: formatted code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,19 @@
 import { closeM, openM } from "./handleRules.js";
+
+
 import { LoadingRules } from "./loadingRules.js";
 
+
 const btn = document.querySelector(".rules");
+
+
 const close = document.querySelector(".close");
 
+
 btn.addEventListener("click", openM.open);
+
+
 close.addEventListener("click", closeM.close);
+
+
 LoadingRules
--- src/scripts/loadingRules.js
+++ src/scripts/loadingRules.js
@@ -1,8 +1,13 @@
 const modal = document.querySelector(".modal__rules");
 
+
 export const LoadingRules = (window.onload = () => {
+
+
   setTimeout(() => {
     modal.style.display = "flex";
     modal.style.transition = ".4s all";
   }, 1000);
+
+  
 });
